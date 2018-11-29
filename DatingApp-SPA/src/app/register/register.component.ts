77a160cece2cfea3d7d32e3bf506166f259726fe
Jose Eyber Garcia - Nexos <josee.garcia@carvajal.com>
2018-11-29 18:30:15 -0500

    Registro de user

---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+      console.log('registration successful');
+  }
+
+  cancel() {
+    console.log('cancelled');
+  }
+
+}
